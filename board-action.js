659b48b6306b9aa01c0e8ae4a321ba59d44b30fb
ciolt <alejandra@cyberscape.co>
2019-09-02 20:13:42 -0400

    update with board actions

---
+++ board-action.js
@@ -0,0 +1,31 @@
+const actions = [
+  'Adopted as amended',
+  'Adopted on first reading',
+  'Adopted',
+  'Forwarded to BCC with a favorable recommendation with committee amendment(s)',
+  'Forwarded to the BCC by the BCC Chairman with a favorable recommendation',
+  'Forwarded to the BCC by the BCC Chairwoman with a favorable recommendation',
+  'Forwarded to the BCC by the BCC Chairperson with a favorable recommendation',
+  'Forwarded to BCC with a favorable recommendation as corrected',
+  'Forwarded to BCC with a favorable recommendation',
+  'Deferred to next committee meeting',
+  'Report Received',
+  'Approved',
+  'Deferred',
+  'Withdrawn'
+]
+
+const getSplitAction = str => {
+  let final = str
+  for (let a = 0; a < actions.length; a += 1) {
+    if (str.indexOf(actions[a]) !== -1) {
+      const astr = str.split(actions[a])
+      astr[1] = actions[a] + astr[1]
+      final = astr
+      break
+    }
+  }
+  return final
+}
+
+module.exports = { actions, getSplitAction }
